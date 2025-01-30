7a130c52cf3e8617e3ac3333e8a67bebfe22b66e
Devanm611 <dmr76@students.uwf.edu>
2025-01-29 21:59:08 -0600

    TCP Server Code From Video

--- httpServer.cpp
+++ httpServer.cpp
@@ -4,12 +4,51 @@
 #include <string>
 #include <cstring>
 #include <thread> 
-#include <netinet/in.h>
-#include <unistd.h>
+
+//TCP Server Libraries
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+#include <unistd.h>
 #include <arpa/inet.h>
+
 using namespace std;
 
+int main(){
+
+    char tcp_server_message[256] = " Hello, I am the TCP Server you successfully connected to!! \n\n        Bye Bye!!!\n\n";
+
+    //create the server socket 
+    int tcp_server_socket;                                 //Socket Descriptor
+    tcp_server_socket = socket(AF_INET, SOCK_STREAM, 0);   //Calling the socket fucntion
+
+    //define the server address
+    struct sockaddr_in tcp_server_address;           //Declaring a structure for the address
+    tcp_server_address.sin_family = AF_INET;         //Sets the address family of the address the client would connect    
+    tcp_server_address.sin_port = htons(39756);      //Passing the port number - converting in right network byte order
+    tcp_server_adress.sin_addr = INADDR_ANY;         //Connecting to 0.0.0.0
+
+    //binding the socket to the IP address and port
+    bind(tcp_server_socket, (struct sockaddr *) &tcp_server_address, sizeof(tcp_server_address)); //Parameters for socket
+
+    //listen for simultaneous connections
+    listen(tcp_server_socket, 5); 
+
+    int tcp_client_socket;
+    tcp_client_socket = accept(tcp_server_socket, NULL, NULL); //Server socket to interact with client
+
+    //send data stream
+    send(tcp_client_socket, tcp_server_message, sizeof(tcp_server_message), 0);
+
+    //close the socket
+    close(tcp_server_socket);
+    
+    return 0;
+
+}
 
 
 
---
+++ http_server.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <fstream>
+#include <string>
